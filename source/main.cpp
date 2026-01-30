acf2c451744db6f4632da1d97d0c8e2e03e42e96
Alexian Hentiu <alexianhentiu1208@gmail.com>
2026-01-30 20:38:54 +0200

    [1.2] OpenGL init

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -11,6 +12,11 @@ int main()
 		return -1;
 	}
 
+	// OpenGL version 3.3 core
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	GLFWwindow *window = glfwCreateWindow(1280, 720, "GameEngine3D", nullptr, nullptr);
 	if (!window) {
 		std::cerr << "Failed to create GLFW window" << std::endl;
@@ -18,8 +24,24 @@ int main()
 		return -1;
 	}
 
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
+
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
