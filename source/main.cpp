a6eaa599bb33b921de2602f2aaa8b7f71f681241
Alexian Hentiu <alexianhentiu1208@gmail.com>
2026-01-30 20:12:53 +0200

    [1.1] GLFW Window init

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,29 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 int main()
 {
+#if defined (__LINUX__)
+	glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "GameEngine3D", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Main loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
     return 0;
 }
