d87f46991494668fcce37d24fa1252fcbcdb53c9
Alexian Hentiu <alexianhentiu1208@gmail.com>
2026-01-30 21:31:52 +0200

    [1.3-1.6] Render triangle

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
@@ -34,12 +36,95 @@ int main()
 		return -1;
 	}
 
+	std::string vertexShaderSource = R"(
+		#version 330 core
+		layout(location = 0) in vec3 position;
+		void main()
+		{
+			gl_Position = vec4(position, 1.0);
+		}
+	)";
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const char* vertexSourceCStr = vertexShaderSource.c_str();
+	glShaderSource(vertexShader, 1, &vertexSourceCStr, nullptr);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED: " << infoLog << std::endl;
+	}
+
+	std::string fragmentShaderSource = R"(
+		#version 330 core
+		out vec4 fragColor;
+		void main()
+		{
+			fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		}
+	)";
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	const char* fragmentSourceCStr = fragmentShaderSource.c_str();
+	glShaderSource(fragmentShader, 1, &fragmentSourceCStr, nullptr);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED: " << infoLog << std::endl;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED: " << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	std::vector<float> vertices = {
+		 0.0f,  0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f
+	};
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind VBO
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind VBO
+	glBindVertexArray(0); // Unbind VAO
+
 	// Main loop
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
